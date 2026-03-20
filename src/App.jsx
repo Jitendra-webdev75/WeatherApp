3e622870228636fec5db3b5bde92b5fea76bdb8a
Jitendra <jitendraphone7505@gmail.com>
2026-03-20 23:43:06 +0530

    making it responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Cards from "./component/Cards.jsx";
 import humidity from "../src/assets/humidity.png";
 import wind from "../src/assets/wind.png";
+import { RiSearchLine } from "@remixicon/react";
 
 function App() {
   const [value, setValue] = useState("");
@@ -10,8 +11,13 @@ function App() {
   const [humd, setHumd] = useState("");
   const [windM, setWindM] = useState("");
   const [isOn, setIson] = useState("");
+  const [cond, setCond] = useState(false);
 
   const getData = async () => {
+    if (value.trim() == "") {
+      alert("🤡 Enter city name");
+      return;
+    }
     const apiKey = "c87452cfea264e3c9f4161806261703";
     const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${value}`;
     const response = await fetch(url);
@@ -22,6 +28,7 @@ function App() {
     setLoc(`${result.location.name}`);
     setHumd(`${result.current.humidity}`);
     setWindM(`${result.current.wind_kph}Kph`);
+    setCond(true);
 
     imgHandler(code);
   };
@@ -47,47 +54,99 @@ function App() {
   };
   return (
     <>
-      <div className="main h-[100vh] w-[100vw] flex  justify-center items-center bg-zinc-800">
+      <div
+        className="main h-[100vh] w-[100vw] flex  justify-center items-center bg-zinc-800
+      "
+      >
         <div
-          className="contentBox  h-[70vh] w-[33vw] bg-emerald-500 rounded-2xl 
-          p-10"
+          className="contentBox  h-[70vh] w-[33vw]    backdrop:blur-md  rounded-2xl 
+          p-10  bg-gradient-to-tr from-zinc-650 via-emerald-750 to-teal-600
+          ring-1 ring-cyan-200 shadow-md shadow-cyan-200
+          sm:h-[70vh] sm:w-[70vw]
+          md:h-[60vh] md:w-[75vw]
+          lg:h-[67vh] lg:w-[60vw]
+          xl:h-[67vh] xl:w-[40vw]
+"
         >
           <div className="searchBar flex items-center ">
             <input
-              onChange={(e) => setValue(e.target.value)}
+              onChange={(e) => {
+                if (e.target.value == "delhi") {
+                  setValue(`${e.target.value},in`);
+                } else {
+                  setValue(e.target.value);
+                }
+              }}
               type="text"
               placeholder="Search City name"
               className="h-[8vh] w-[32vw] bg-amber-100 p-2 text-2xl outline-0 
-              rounded-l-xl"
+              rounded-l-xl 
+              sm:h-[8vh] sm:w-[48vw] sm:text-3xl sm:p-4
+              md:h-[8vh] md:w-[52vw] md:p-3 md:text-4xl
+              lg:h-[10vh] lg:w-[52vw] lg:p-3 lg:text-5xl
+              xl:h-[10vh] xl:w-[45vw] xl:p-3 xl:text-3xl"
             />
             <button
               onClick={getData}
-              className="h-[8vh] w-[11vw] bg-blue-800 text-white rounded-r-2xl
-              active:scale-95"
+              className="h-[8vh] w-[8vw] flex justify-center items-center  text-white rounded-r-2xl shadow-sm shadow-black 
+              active:scale-95 bg-gradient-to-br from-slate-700 via-[#06102a] to-[#020617] 
+              hover::bg-gradient-to-br hover:from-cyan-700 hover:via-[#3354a9] hover:to-[#05091b] hover:text-[#000] 
+              
+              sm:h-[8vh] sm:w-[10vw]
+              md:h-[8vh] md:w-[12vw]
+              lg:h-[10vh] lg:w-[10vw] "
             >
-              Search
+              <RiSearchLine />
             </button>
           </div>
 
-          <div className="weathDetail flex items-center gap-2 pt-10   ">
+          <div
+            className="weathDetail flex items-center gap-2 pt-10 
+          sm:h-[37vh] sm:[40vw] sm:flex sm:flex-col-reverse  sm:items-center sm:pt-8  
+          md:h-[27vh] md:[42vw] md:flex  md:flex-row md:items-center md:p-10 
+          lg:h-[30vh] lg:[54vw] lg:flex lg:items-center lg:p-10  "
+          >
             <div className="weatHead flex flex-col gap-5  p-1">
-              <h1 className="text-6xl  text-amber-50">{temp}</h1>
-              <h2 className="text-4xl  text-amber-50">{loc}</h2>
+              <h1
+                className="text-6xl  text-amber-50
+              sm:text-[4rem] md:text-[5rem] lg:text-[6rem] xl:text-[4rem]"
+              >
+                {temp}
+              </h1>
+              <h2
+                className="text-4xl  text-amber-50
+              sm:text-[3rem] md:text-[4rem] lg:text-[4rem] xl:text-[3rem]"
+              >
+                {loc}
+              </h2>
             </div>
-            <div className="images h-[26vh] w-[50vw]  object-contain ">
+            <div
+              className="images h-[26vh] w-[50vw]  object-contain 
+            "
+            >
               {isOn && (
                 <img
                   src={`./src/assets/${isOn}.png`}
                   className="h-[26vh] w-[17vw]  
-                  "
+                    sm:h-[27vh] sm:w-[60vw]  
+                    md:h-[26vh] md:w-[39vw] 
+                    lg:h-[30vh] lg:w-[40vw]
+                    xl:h-[30vh] xl:w-[20vw]"
                 />
               )}
             </div>
           </div>
-          <div className="cardDiv flex gap-6 pt-7">
-            <Cards value={humd} icon={humidity} />
-            <Cards value={windM} icon={wind} />
-          </div>
+          {cond ? (
+            <div className="cardDiv flex block gap-6 pt-7">
+              <Cards value={humd} icon={humidity} />
+              <Cards value={windM} icon={wind} />
+            </div>
+          ) : (
+            <div className="cardDiv flex hidden  gap-6 pt-7">
+              <Cards value={humd} icon={humidity} labelVal={"Humidity"} />
+              <Cards value={windM} icon={wind} labelVal={"Wind"} />
+            </div>
+          )}
         </div>
       </div>
     </>
--- src/component/Cards.jsx
+++ src/component/Cards.jsx
@@ -1,11 +1,27 @@
-function Cards({ value, icon }) {
+function Cards({ value, icon, labelVal }) {
   return (
     <>
-      <div className="card h-[13vh] w-[13vw] flex items-center  justify-around bg-blue-400 gap-2 ">
+      <div
+        className="card h-[13vh] w-[13vw] flex items-center  justify-around bg-gradient-to-r from-blue-400  to-blue-600 gap-2 
+        shadow-md shadow-amber-100 rounded-xl 
+        hover::bg-gradient-to-r hover:from-red-400  hover:to-blue-600
+        sm:h-[10vh] sm:w-[30vw] sm:mt-2
+        md:h-[10vh] md:[14vw]
+        lg:h-[10vh] lg:[14vw] lg:mt-4
+        xl:h-[10vh] xl:[14vw] xl:mt-7"
+      >
         <div className="img h-12 w-12">
           <img src={icon} />
         </div>
-        <h1 className="text-2xl text-zinc-900">{value}</h1>
+        <h1
+          className="text-2xl text-white 
+          sm:text-[2.2rem]
+          md:text-[3rem]
+          lg:text-[2.5rem]
+          xl:text-[2rem] "
+        >
+          {value}
+        </h1>
       </div>
     </>
   );
