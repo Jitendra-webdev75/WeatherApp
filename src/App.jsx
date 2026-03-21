1c51ee291a38eda479af5f87da360dc4427f589c
Jitendra <jitendraphone7505@gmail.com>
2026-03-21 19:16:12 +0530

    fixed layout issues

--- src/App.jsx
+++ src/App.jsx
@@ -55,20 +55,20 @@ function App() {
   return (
     <>
       <div
-        className="main h-[100vh] w-[100vw] flex  justify-center items-center bg-zinc-800
-      "
+        className="main h-[100vh] w-[100vw] flex  justify-center items-center 
+        overflow-hidden bg-[radial-gradient(circle_farthest-corner_at_24.1%_68.8%,_rgba(50,50,50,1)_0%,_rgba(0,0,0,1)_99.4%)]"
       >
         <div
-          className="contentBox  h-[72vh] w-[52vw] flex flex-col   backdrop:blur-md  rounded-2xl 
+          className="contentBox  h-[72vh] w-[80vw] flex flex-col   backdrop:blur-md  rounded-2xl 
           p-10  bg-gradient-to-tr from-zinc-650 via-emerald-750 to-teal-600
-          ring-1 ring-cyan-200 shadow-md shadow-cyan-200
+          ring-1 ring-cyan-200 shadow-lg shadow-cyan-200
           sm:h-[73vh] sm:w-[70vw] sm:flex sm:flex-col sm:gap-6
           md:h-[63vh] md:w-[75vw]
           lg:h-[73vh] lg:w-[60vw]
           xl:h-[69vh] xl:w-[40vw]
 "
         >
-          <div className="searchBar flex items-center ">
+          <div className="searchBar  flex justify-center ">
             <input
               onChange={(e) => {
                 if (e.target.value == "delhi") {
@@ -79,8 +79,8 @@ function App() {
               }}
               type="text"
               placeholder="Search City name"
-              className="h-[8vh] w-[32vw] bg-amber-100 p-2 text-2xl outline-0 
-              rounded-l-xl 
+              className="h-[8vh] w-[50vw] bg-amber-100 p-2 text-xl outline-0 
+              rounded-l-xl font-semibold
               sm:h-[8vh] sm:w-[48vw] sm:text-3xl sm:p-4
               md:h-[8vh] md:w-[52vw] md:p-3 md:text-4xl
               lg:h-[10vh] lg:w-[44vw] lg:p-3 lg:text-3xl
@@ -88,7 +88,7 @@ function App() {
             />
             <button
               onClick={getData}
-              className="h-[8vh] w-[8vw] flex justify-center items-center  text-white rounded-r-2xl shadow-sm shadow-black 
+              className="h-[8vh] w-[30vw] flex justify-center items-center  text-white rounded-r-2xl shadow-sm shadow-black 
               active:scale-95 bg-gradient-to-br from-slate-700 via-[#06102a] to-[#020617] 
               hover::bg-gradient-to-br hover:from-cyan-700 hover:via-[#3354a9] hover:to-[#05091b] hover:text-[#000] 
               
@@ -107,29 +107,33 @@ function App() {
           md:h-[27vh] md:[42vw] md:flex  md:flex-row md:items-center md:p-10 
           lg:h-[30vh] lg:[54vw] lg:flex lg:items-center lg:p-10  "
           >
-            <div className="weatHead flex flex-col content-center gap-5  p-1">
+            <div
+              className="weatHead flex flex-col content-center gap-5  p-1
+              "
+            >
               <h1
-                className="text-6xl  text-amber-50
+                className="text-[2.6rem]  text-amber-50 text-shadow-md
+                text-shadow-green-300
               sm:text-[4rem] md:text-[5rem] lg:text-[4rem] xl:text-[4rem]"
               >
                 {temp}
               </h1>
               <h2
-                className="text-4xl  text-amber-50
-              sm:text-[3rem] md:text-[4rem] lg:text-[3.2rem] xl:text-[3rem]"
+                className="text-4xl  text-amber-50 text-shadow-md
+                text-shadow-green-300  sm:text-[3rem] md:text-[4rem] lg:text-[3.2rem] xl:text-[3rem]"
               >
                 {loc}
               </h2>
             </div>
             <div
-              className="images h-[20vh] w-[40vw]  object-contain  flex
-              items-center 
+              className="images h-[20vh] w-[70vw]  object-contain  flex
+              items-center  justify-center 
             "
             >
               {isOn && (
                 <img
                   src={`./src/assets/${isOn}.png`}
-                  className="h-[26vh] w-[40vw] mt-5  
+                  className="h-[18vh] w-[40vw] mt-5   
                     sm:h-[20vh] sm:w-[56vw]   
                     md:h-[26vh] md:w-[33vw] 
                     lg:h-[30vh] lg:w-[40vw]
@@ -139,7 +143,7 @@ function App() {
             </div>
           </div>
           {cond ? (
-            <div className="cardDiv flex  block gap-6 pt-7">
+            <div className="cardDiv  flex  items-center block gap-6 mt-5   ">
               <Cards value={humd} icon={humidity} />
               <Cards value={windM} icon={wind} />
             </div>
